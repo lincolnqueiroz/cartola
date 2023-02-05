2486fd9462d84c1fbdd5ba6d69060976c56a3744
rafaelpfranco <rafaelpf9@gmail.com>
2023-02-05 18:11:30 -0300

    Página de login

--- frontend/src/Links.jsx
+++ frontend/src/Links.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import { Routes, Route} from 'react-router-dom'
 import Homepage from './pages/Homepage'
+import LoginPage from './pages/Loginpage'
 import NotFound from './pages/NotFound'
 
 function Links() {
@@ -8,8 +9,8 @@ function Links() {
   return (
     <Routes>
         <Route path='*' element={<NotFound/>} />
-
         <Route exact path='/' element={<Homepage/>} />
+        <Route exact path='/login' element={<LoginPage/>} />
     </Routes>
   )
 }
